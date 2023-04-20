9de91fe3a1c54a5cc1bbc78cfc468e943f9e0b34
t-montes <t.montes@uniandes.edu.co>
2023-04-20 12:17:13 -0500

    update CSV importing & app processing to allow all CSVs

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 import json
 import joblib
 import pandas as pd
+from io import StringIO
 
 usage_count = 0
 pipeline = joblib.load('best_model.joblib')
@@ -11,7 +12,12 @@ pipeline.predict(pd.Series(['']))
 
 def feel_extractor(texts, include_texts=False):
     global pipeline
-    df = pd.DataFrame(texts, columns=['texto'])
+    # si no es un DF, lo convierto a uno
+    if not isinstance(texts, pd.DataFrame):
+        df = pd.DataFrame(texts, columns=['texto'])
+    else:
+        df = texts.copy()
+        df = df[['texto']]
     df['sentimiento'] = pipeline.predict(df['texto'])
     df['sentimiento'] = df['sentimiento'].replace({1: 'positivo', 0: 'negativo'})
     return df.to_dict('records') if include_texts else df['sentimiento'].to_list()
@@ -45,6 +51,31 @@ def main_endpoint_json(request):
         return JsonResponse(response_data)
     else:
         return JsonResponse({'error': 'POST request required'})
+
+@csrf_exempt
+def main_endpoint_csvtext(request):
+    global usage_count
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        texts = data['textos'] # raw text "col1,col2,col3\n1,2,3\n4,5,6"
+        sep = data['separador'] if 'separador' in data else ','
+        col_name = data['nombre_columna'] if 'nombre_columna' in data else 'texto'
+        include_texts = data['incluir_textos'] if 'incluir_textos' in data else False
+
+        df = pd.read_csv(StringIO(texts), sep=sep)
+        print(df)
+
+        if col_name not in df.columns:
+            return JsonResponse({'error': f'Columna {col_name} no encontrada'})
+        df.rename(columns={col_name: 'texto'}, inplace=True)
+
+        feelings = feel_extractor(df, include_texts)
+        response_data = {'sentimiento': feelings}
+        usage_count += 1
+        return JsonResponse(response_data)
+    else:
+        return JsonResponse({'error': 'POST request required'})
+
 
 @csrf_exempt
 def usage_endpoint(request):
--- prj/urls.py
+++ prj/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from app.views import main_endpoint, main_endpoint_json, usage_endpoint
+from app.views import main_endpoint, main_endpoint_json, main_endpoint_csvtext, usage_endpoint
 
 app_name = 'app'
 
 urlpatterns = [
     path('', main_endpoint, name='main_endpoint'),
     path('json/', main_endpoint_json, name='main_endpoint_json'),
+    path('csvtext/', main_endpoint_csvtext, name='main_endpoint_csvtext'),
     path('usage/', usage_endpoint, name='usage_endpoint')
 ]
